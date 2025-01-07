d5072555f2c0aa83a27242f52e7940260f4bb30b
leanne ashley reyes <leanneashleyreyes@gmail.com>
2025-01-07 10:20:38 +0800

    upadat

---
+++ app/app/Http/Controllers/EmployeeAttendanceController.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\http\Request;
+use App\models\User;
+use Illuminate\Routing\ControllerDispatcher;
+
+class EmployeeAttendanceController extends Controllers{
+    
+}
--- app/resources/views/employee/create.blade.php
+++ app/resources/views/employee/create.blade.php
@@ -1,29 +1,29 @@
-<!-- @extends('layouts.app')
+    <!-- @extends('layouts.app')
 
-@section('content') -->
- 
+    @section('content') -->
+    
 
-    <div class="container">
-        <h2>Add Employee Attendance</h2>
-        <form method="post" action="{{ route('employee.create') }}">
-            @csrf
-            <div class="form-group">
-                <label for="employee_id">Employee ID</label>
-                <input type="number" name="employee_id" class="form-control" id="employee_id">
-            </div>
-            <div class="form-group">
-                <label for="date">Date</label>
-                <input type="date" name="date" class="form-control" id="date">
-            </div>
-            <div class="form-group">
-                <label for="check_in">Check In</label>
-                <input type="time" name="check_in" class="form-control" id="check_in">
-            </div>
-            <div class="form-group">
-                <label for="check_out">Check Out</label>
-                <input type="time" name="check_out" class="form-control" id="check_out">
-            </div>
-            <button type="submit" class="btn btn-primary">Save Attendance</button>
-        </form>
-    </div>
+        <div class="container">
+            <h2>Add Employee Attendance</h2>
+            <form method="post" action="{{ route('employee.create') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="employee_id">Employee ID</label>
+                    <input type="number" name="employee_id" class="form-control" id="employee_id">
+                </div>
+                <div class="form-group">
+                    <label for="date">Date</label>
+                    <input type="date" name="date" class="form-control" id="date">
+                </div>
+                <div class="form-group">
+                    <label for="check_in">Check In</label>
+                    <input type="time" name="check_in" class="form-control" id="check_in">
+                </div>
+                <div class="form-group">
+                    <label for="check_out">Check Out</label>
+                    <input type="time" name="check_out" class="form-control" id="check_out">
+                </div>
+                <button type="submit" class="btn btn-primary">Save Attendance</button>
+            </form>
+        </div>
 
