8ea349b8d7438a70ef7293fa3e81c966716a128a
leanne ashley reyes <leanneashleyreyes@gmail.com>
2025-01-07 09:33:49 +0800

    user

---
+++ app/resources/views/employee/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h2>Add employee Attendance</h2>
+        <form method="post" action="{{ route('absensi_sholat.store') }}">
+            @csrf
+            <div class="form-group">
+                <label for="time">Time</label>
+                <input type="datetime-local" name="time" class="form-control" id="time">
+            </div>
+            <div class="form-group">
+                <label for="date">Date</label>
+                <input type="date" name="date" class="form-control" id="date">
+            </div>
+            <div class="form-group">
+                <label for="prayer_schedule_id">Prayer Schedule</label>
+                <input type="number" name="prayer_schedule_id" class="form-control" id="prayer_schedule_id">
+            </div>
+            <div class="form-group">
+                <label for="id_devices">Device</label>
+                <input type="number" name="id_devices" class="form-control" id="id_devices">
+            </div>
+            <div class="form-group">
+                <label for="nis_santri">NIS Santri</label>
+                <input type="text" name="nis_santri" class="form-control" id="nis_santri">
+            </div>
+            <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+    </div>
+@endsection
