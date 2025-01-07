857723b8f5d79f146afa9c4945fdf3da05232f61
leanne ashley reyes <leanneashleyreyes@gmail.com>
2025-01-07 16:15:45 +0800

    ,

--- app/app/Http/Controllers/EmployeeAttendanceController.php
+++ app/app/Http/Controllers/EmployeeAttendanceController.php
@@ -6,6 +6,7 @@ use Illuminate\http\Request;
 use App\models\User;
 use Illuminate\Routing\ControllerDispatcher;
 
+
 class EmployeeAttendanceController extends Controllers{
     public function create(){
         return view("employee.create");
