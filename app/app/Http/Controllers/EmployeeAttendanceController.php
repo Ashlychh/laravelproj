6077fb8aa5aee4a487645a5a36f61624df3e99b0
leanne ashley reyes <leanneashleyreyes@gmail.com>
2025-01-07 10:47:03 +0800

    .

--- app/app/Http/Controllers/EmployeeAttendanceController.php
+++ app/app/Http/Controllers/EmployeeAttendanceController.php
@@ -7,5 +7,19 @@ use App\models\User;
 use Illuminate\Routing\ControllerDispatcher;
 
 class EmployeeAttendanceController extends Controllers{
+    public function create(){
+        return view("employee.create");
+    }
+    public function store(Request $Request){
+         User::create([
+            'employee_id'=> $request -> employee_id,
+            'date' => $request -> date,
+            'check_in' => $request -> check_in,
+            'check_out' => $request -> check_out,
+        ]);
+
+        return redirect()->route ("employee.create");
+    }
+      
     
 }
--- app/app/Models/User.php
+++ app/app/Models/User.php
@@ -18,9 +18,10 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'employee_id',
+        'date',
+        'check_in',
+        'check_out',
     ];
 
     /**
