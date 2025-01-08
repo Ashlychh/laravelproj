08f68e80dad546fb97ab10d5a22d5c8a59cc81ed
Jandyjade <reyesjandy.basc@gmail.com>
2025-01-08 18:01:50 +0800

    .

--- app/app/Models/Attendance.php
+++ app/app/Models/Attendance.php
@@ -16,4 +16,3 @@ class Attendance extends Model
         'check_out',
     ];
 }
-
--- app/routes/web.php
+++ app/routes/web.php
@@ -7,88 +7,50 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AttendanceController;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
+|--------------------------------------------------------------------------|
+| Web Routes                                                                |
+|--------------------------------------------------------------------------|
+| Here is where you can register web routes for your application.           |
+| These routes are loaded by the RouteServiceProvider and all of them will  |
+| be assigned to the "web" middleware group. Make something great!          |
+|--------------------------------------------------------------------------|
 */
 
-
+// Authentication Routes
 Route::get('signup', [AuthController::class, 'create'])->name('employee.signup');  // Show signup form
 Route::post('signup', [AuthController::class, 'store'])->name('signup.store');     // Handle signup form submission
 
-Route::get('login', [AuthController::class, 'showLoginForm'])->name('employee.login');       // Show login form
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('employee.login');  // Show login form
+// Route::post('logout', [AuthController::class, 'logout'])->name('logout');    // Handle logout (uncomment if you need it)
 
+// Employee Attendance Routes
 Route::prefix('employee/attendance')->name('employee.attendance.')->group(function() {
-    Route::post('/', [AttendanceController::class, 'index'])->name('index');
-    Route::get('create', [AttendanceController::class, 'create'])->name('create');
-    Route::post('/', [AttendanceController::class, 'store'])->name('store');
-    Route::get('{id}/edit', [AttendanceController::class, 'edit'])->name('edit');
-    // Route::put('{id}', [AttendanceController::class, 'update'])->name('update');
-    // Route::delete('{id}', [AttendanceController::class, 'destroy'])->name('destroy');
+    Route::get('/', [AttendanceController::class, 'index'])->name('index');      // List attendance records
+    Route::get('create', [AttendanceController::class, 'create'])->name('create'); // Show form to create new attendance
+    Route::post('/', [AttendanceController::class, 'store'])->name('store');     // Store new attendance
+    Route::get('{id}/edit', [AttendanceController::class, 'edit'])->name('edit'); // Edit an attendance record
+    Route::put('{id}', [AttendanceController::class, 'update'])->name('update');  // Update attendance record (uncomment if needed)
+    Route::delete('{id}', [AttendanceController::class, 'destroy'])->name('destroy'); // Delete an attendance record (uncomment if needed)
 });
 
+// Device Routes
+Route::prefix('devices')->name('devices.')->group(function() {
+    Route::get('/', [DeviceController::class, 'index'])->name('index');  // List devices
+    Route::get('device-log', [DeviceController::class, 'deviceLog'])->name('deviceLog');  // View device logs
+    Route::get('finger-log', [DeviceController::class, 'fingerLog'])->name('fingerLog');  // View finger logs
+    Route::get('attendance', [DeviceController::class, 'attendance'])->name('attendance');  // View attendance data
+});
 
-// Route::post('logout', [AuthController::class, 'logout'])->name('logout');           // Handle logout
+// iClock Routes for Device Communication
+Route::prefix('iclock')->name('iclock.')->group(function() {
+    Route::get('cdata', [IclockController::class, 'handshake'])->name('handshake');  // Handshake with the device
+    Route::post('cdata', [IclockController::class, 'receiveRecords'])->name('receiveRecords');  // Receive records from the device
+    Route::get('test', [IclockController::class, 'test'])->name('test');  // Test connection
+    Route::get('getrequest', [IclockController::class, 'getrequest'])->name('getrequest');  // Handle request from the device
+});
 
-
-//device
-
-Route::get('device', [DeviceController::class,'Index'])->name('devices.index');
-Route::get('devices-log', [DeviceController::class,'DeviceLog'])->name ('devices.DeviceLog');
-Route::get('finger-log', [DeviceController::class, 'FingerLog'])->name('devices.FingerLog');
-Route::get('attendance', [DeviceController::class, 'Attendance'])->name('devices.Attendance');
-
-
-//leanne
-
+// Welcome Route (Default Landing Page)
 Route::get('/', function () {
     return view('welcome');
 });
-
-// handshake
-Route::get('/iclock/cdata', [iclockController::class, 'handshake']);
-
-// request dari device
-Route::post('/iclock/cdata', [iclockController::class, 'receiveRecords']);
-
-Route::get('/iclock/test', [iclockController::class, 'test']);
-Route::get('/iclock/getrequest', [iclockController::class, 'getrequest']);
-
-
-// Route::post('/logIn', function () {
-//     // Handle form submission
-// })->name('employee.attendance.create');
-
-// Route::get('/edit', function () {
-//     return view('employee.attendance.create');
-// })->name('employee.attendance.update');
-
-// Route::post('/edit', function () {
-//     // Handle form submission
-// })->name('employee.attendance.update');
-
-
-// use App\Http\Controllers\AttendanceController;
-
-// Route::get('employee/attendance/create', [AttendanceController::class, 'create'])->name('employee.attendance.create');
-// Route::post('employee/attendance', [AttendanceController::class, 'store'])->name('employee.attendance.store');
-// // Route::get('employee/attendance', [AttendanceController::class, 'show'])->name('employee.attendance.index');
-// // Route::post(uri: 'employee/attendance/index', [AttendanceController::class, 'show'])->name('employee.attendance.index');
-
-
-// Route::get('employee/attendance/index', function () {
-//     // Handle form submission
-// })->name('employee.attendance.index');
-
-
-// Route::post('employee/attendance/index', function () {
-//     // Handle form submission
-// })->name('employee.attendance.index');
-
-
 
