b86d0bf80e6a5c538138f0bda8c6f57a51950788
Jandyjade <reyesjandy.basc@gmail.com>
2025-01-08 14:18:06 +0800

    Update AuthController.php

--- app/app/Http/Controllers/AuthController.php
+++ app/app/Http/Controllers/AuthController.php
@@ -6,8 +6,6 @@ use illuminate\support\Facades\Hash;
 
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
