97ef82d1f36018d54ebefcfc1081bfa4efb31dbe
leanne ashley reyes <leanneashleyreyes@gmail.com>
2025-01-08 15:04:32 +0800

    ew

--- app/app/Http/Controllers/AuthController.php
+++ app/app/Http/Controllers/AuthController.php
@@ -10,13 +10,13 @@ use App\Models\User;
 class AuthController extends Controller
 {
     // Show signup form
-    public function showSignUpForm()
+    public function create()
     {
         return view('employee.signup');
     }
 
     // Handle signup form submission
-    public function signUp(Request $request)
+    public function store(Request $request)
     {
         $request->validate([
             'name' => 'required|string|max:255',
@@ -36,32 +36,77 @@ class AuthController extends Controller
         return redirect()->route('employee.login');
     }
 
-    // Handle login form submission
-    public function logIn(Request $request)
+    //read
+    public function show(User $user)
     {
-        $validator = Validator::make($request->all(), [
-            'email' => 'required|email',
-            'password' => 'required',
-        ]);
+        return view("employee.attendance.show",compact("employee.attendance"));
 
+    }
+
+    //edit
+    public function edit(User $user)
+    {
+        return view('employee.attendance.edit',compact('employee.attendance'));
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $validator = Validator::make($request->all(),
+        [ 
+            'email' => 'required|string|max:255',
+            'name' => 'required|email|unique:user,email',$user->id,
+            'password' => 'nullabele|min6|confirmed',
+            
+        ]);
+        
         if ($validator->fails()) {
             return redirect()->route('login')
-                             ->withErrors($validator)
-                             ->withInput();
-        }
-
-        if (Auth::attempt($request->only('email', 'password'))) {
-            // If successfully login, redirect to dashboard.
-            return redirect()->route('home');
-        }
-
-        return redirect()->route('login')->withErrors(['email' => 'Invalid credentials.']);
+             ->withErrors($validator)
+             ->withInput();
+                    }
+            
+        $user -> update([
+            'email'=> $request-> email,
+            'name'=> $request->name,
+            'password' => Hash::make($request->password),
+        ]);
     }
-
-    // Handle logout
-    public function logOut(Request $request)
+    public function destroy(User $user)
     {
-        Auth::logout();
-        return redirect()->route('login');
+        $user->delete();
+        return redirect()->route('employee.attendance.index');
     }
 }
+
+
+
+//     public function 
+//     // Handle login form submission
+//     public function logIn(Request $request)
+//     {
+//         $validator = Validator::make($request->all(), [
+//             'email' => 'required|email',
+//             'password' => 'required',
+//         ]);
+
+//         if ($validator->fails()) {
+//             return redirect()->route('login')
+//                              ->withErrors($validator)
+//                              ->withInput();
+//         }
+
+//         if (Auth::attempt($request->only('email', 'password'))) {
+//             // If successfully login, redirect to dashboard.
+//             return redirect()->route('home');
+//         }
+
+//         return redirect()->route('login')->withErrors(['email' => 'Invalid credentials.']);
+//     }
+
+//     // Handle logout
+//     public function logOut(Request $request)
+//     {
+//         Auth::logout();
+//         return redirect()->route('login');
+//     }
+// }
