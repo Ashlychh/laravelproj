685979f27f4c516f3ae9d1dd34d3c1f975d2f2dc
leanne ashley reyes <leanneashleyreyes@gmail.com>
2025-01-08 11:50:17 +0800

    Update create.blade.php

--- app/resources/views/employee/attendance/create.blade.php
+++ app/resources/views/employee/attendance/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+<!-- @extends('layouts.app')
 
 @section('content')
     <div class="container">
@@ -24,4 +24,32 @@
             <button type="submit" class="btn btn-primary">Save Attendance</button>
         </form>
     </div>
+@endsection -->
+
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <h2>Add Device</h2>
+        <!-- <form method="post" action="{{ route('devices.store') }}"> -->
+            <form method="post" action="{{ route('employee.attendance.create') }}">
+            @csrf
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" name="name" class="form-control" id="name" placeholder="Name">
+            </div>
+            <div class="form-group">
+                <label for="serialNum">Serial Number</label>
+                <input type="text" name="serialNum" class="form-control" id="serialNum" placeholder="Serial Number">
+            </div>
+            <div class="form-group">
+                <label for="location">Location</label>
+                <input type="text" name="location" class="form-control" id="location" placeholder="Location">
+            </div>
+
+            <button type="submit" class="btn btn-primary">Save</button>
+        </form>
+    </div>
 @endsection
+
