eea2505d0b94ea8de2bb188919d85481fbdaddf7
leanne ashley reyes <leanneashleyreyes@gmail.com>
2025-01-08 13:52:53 +0800

    Update AuthController.php

--- app/app/Http/Controllers/AuthController.php
+++ app/app/Http/Controllers/AuthController.php
@@ -18,7 +18,28 @@ class AuthController extends Controller
     {
         $user = User::create([
             "email" => $request -> email,
-            ""
+            "name" => $request -> name,
+            "password" => Hash::make($request -> password),
         ])
+        Auth::login($user);
+
+    //return to homepage/dashboard
+        return redirect()-> route("home");
+    }
+
+    public function logIn(request $request){
+        $user = $validation::make($request->all(),
+        [
+            "email"-> $request -> email,
+            "password" -> required,
+        ]);
+
+
+        if ($validator -> fails()){
+            return redirect() -> route('logIn')
+            ->withErrors($validator)
+            ->withInput();
+            
+        }
     }
 }
