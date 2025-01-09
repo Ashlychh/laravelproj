f0dc05317ca4e1d6d18b9c1f3773418b5aaa34cd
leanne ashley reyes <leanneashleyreyes@gmail.com>
2025-01-09 14:48:41 +0800

    Update LoginController.php

--- app/app/Http/Controllers/LoginController.php
+++ app/app/Http/Controllers/LoginController.php
@@ -26,7 +26,7 @@
             // Attempt to log the user in
             if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
                 // Authentication was successful, redirect to the intended page or dashboard
-                return redirect()->intended('employee.home');
+                return redirect()->intended('home');
             } else {
                 // Authentication failed
                 return back()->withErrors([
