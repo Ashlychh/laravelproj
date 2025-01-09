ffea8adc270d65cb02273154e5ce92abe35cc74b
leanne ashley reyes <leanneashleyreyes@gmail.com>
2025-01-09 14:21:10 +0800

    Update web.php

--- app/routes/web.php
+++ app/routes/web.php
@@ -36,29 +36,29 @@ Route::post('/signup', [SignupController::class, 'store'])->name('signup.store')
 Route::get('employee/login', [LoginController::class, 'showLoginForm'])->name('employee.login');
 Route::post('employee/login', [LoginController::class, 'login'])->name('login.submit');
 
-// // Employee Attendance Routes
-// Route::get('/', [AttendanceController::class, 'index'])->name('index');  // List attendance records
-// Route::get('create', [AttendanceController::class, 'create'])->name('create');  // Show form to create new attendance
-// Route::post('/', [AttendanceController::class, 'store'])->name('store');  // Store new attendance
-// Route::get('{id}/edit', [AttendanceController::class, 'edit'])->name('edit');  // Edit an attendance record
-// Route::put('{id}', [AttendanceController::class, 'update'])->name('update');  // Update attendance record
-// // Route::delete('{id}', [AttendanceController::class, 'destroy'])->name('destroy');  // Delete an attendance record
+// Employee Attendance Routes
+Route::get('/', [AttendanceController::class, 'index'])->name('index');  // List attendance records
+Route::get('create', [AttendanceController::class, 'create'])->name('create');  // Show form to create new attendance
+Route::post('/', [AttendanceController::class, 'store'])->name('store');  // Store new attendance
+Route::get('{id}/edit', [AttendanceController::class, 'edit'])->name('edit');  // Edit an attendance record
+Route::put('{id}', [AttendanceController::class, 'update'])->name('update');  // Update attendance record
+Route::delete('{id}', [AttendanceController::class, 'destroy'])->name('destroy');  // Delete an attendance record
 
 
-// // Device Routes
-// Route::prefix('devices')->name('devices.')->group(function() {
-//     Route::get('/', [DeviceController::class, 'index'])->name('index');  // List devices
-//     Route::get('device-log', [DeviceController::class, 'deviceLog'])->name('deviceLog');  // View device logs
-//     Route::get('finger-log', [DeviceController::class, 'fingerLog'])->name('fingerLog');  // View finger logs
-//     Route::get('attendance', [DeviceController::class, 'attendance'])->name('attendance');  // View attendance data
-// });
+// Device Routes
+Route::prefix('devices')->name('devices.')->group(function() {
+    Route::get('/', [DeviceController::class, 'index'])->name('index');  // List devices
+    Route::get('device-log', [DeviceController::class, 'deviceLog'])->name('deviceLog');  // View device logs
+    Route::get('finger-log', [DeviceController::class, 'fingerLog'])->name('fingerLog');  // View finger logs
+    Route::get('attendance', [DeviceController::class, 'attendance'])->name('attendance');  // View attendance data
+});
 
-// // iClock Routes for Device Communication
-// Route::prefix('iclock')->name('iclock.')->group(function() {
-//     Route::get('cdata', [IclockController::class, 'handshake'])->name('handshake');  // Handshake with the device
-//     Route::post('cdata', [IclockController::class, 'receiveRecords'])->name('receiveRecords');  // Receive records from the device
-//     Route::get('test', [IclockController::class, 'test'])->name('test');  // Test connection
-//     Route::get('getrequest', [IclockController::class, 'getrequest'])->name('getrequest');  // Handle request from the device
-// });
+// iClock Routes for Device Communication
+Route::prefix('iclock')->name('iclock.')->group(function() {
+    Route::get('cdata', [IclockController::class, 'handshake'])->name('handshake');  // Handshake with the device
+    Route::post('cdata', [IclockController::class, 'receiveRecords'])->name('receiveRecords');  // Receive records from the device
+    Route::get('test', [IclockController::class, 'test'])->name('test');  // Test connection
+    Route::get('getrequest', [IclockController::class, 'getrequest'])->name('getrequest');  // Handle request from the device
+});
 
 
